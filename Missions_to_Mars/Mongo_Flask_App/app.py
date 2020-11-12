9dc08f47cd04ec7f64f3eb04cc135a27b12d02b8
Stanley Tan <stanley.tan19@gmail.com>
2020-11-11 21:53:42 -0800

    building on hw

---
+++ Missions_to_Mars/Mongo_Flask_App/app.py
@@ -0,0 +1,38 @@
+# import necessary libraries
+from flask import Flask, render_template
+
+# create instance of Flask app
+app = Flask(__name__)
+
+# List of dictionaries
+dogs = [{"name": "Fido", "type": "Lab"},
+        {"name": "Rex", "type": "Collie"},
+        {"name": "Suzzy", "type": "Terrier"},
+        {"name": "Tomato", "type": "Retriever"}]
+
+image_files1 = [{'title': 'Valles Marineris',
+  'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/valles_marineris_enhanced.tif/full.jpg'},
+ {'title': 'Cerberus Hemisphere',
+  'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif/full.jpg'}]
+
+image_files2 = [{'title': 'Schiaparelli Hemisphere', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/schiaparelli_enhanced.tif/full.jpg'},
+ {'title': 'Syrtis Major',
+  'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/syrtis_major_enhanced.tif/full.jpg'}]
+
+
+
+# create route that renders index.html template
+@app.route("/")
+def index():
+
+    return render_template("index.html", image_files=image_files)
+
+@app.route("/images")
+def second_page_images():
+
+    return render_template("images.html", image_files1=image_files1, image_files2=image_files2, )
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
